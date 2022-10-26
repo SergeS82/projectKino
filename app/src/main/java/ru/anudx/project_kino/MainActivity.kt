52693db37b89aacd0b304f07a2fe56e49a57393e
SergeS82 <bazantmail@gmail.com>
2022-10-26 22:21:40 +0300

    setOnMenuItemClickListener

--- app/src/main/java/ru/anudx/project_kino/MainActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.anudx.project_kino
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,13 +11,27 @@ import ru.anudx.project_kino.databinding.BarLayoutTestBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val bam = ActivityMainBinding.inflate(layoutInflater)
-        val bam = BarLayoutTestBinding.inflate(layoutInflater)
-        setContentView(bam.root)
-//        bam.btnMenu.setOnClickListener { Toast.makeText(this, R.string.btn_menu, Toast.LENGTH_SHORT).show() }
-//        bam.btnFavorites.setOnClickListener { Toast.makeText(this, R.string.btn_favorites, Toast.LENGTH_SHORT).show() }
-//        bam.btnLate.setOnClickListener { Toast.makeText(this, R.string.btn_later, Toast.LENGTH_SHORT).show() }
-//        bam.btnSelections.setOnClickListener { Toast.makeText(this, R.string.btn_selections, Toast.LENGTH_SHORT).show() }
-//        bam.btnSettings.setOnClickListener { Toast.makeText(this, R.string.btn_settings, Toast.LENGTH_SHORT).show() }
+        val bam = ActivityMainBinding.inflate(layoutInflater)
+        val bblt = BarLayoutTestBinding.inflate(layoutInflater)
+        setContentView(bblt.root)
+        bblt.topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Когда-нибудь сдесь будет навигация ...", Toast.LENGTH_SHORT).show() }
+        bblt.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.fav -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.more ->{
+                    Toast.makeText(this, "Ещё", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else ->false
+            }
+        }
     }
 }
