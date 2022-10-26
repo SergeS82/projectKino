637e4192a16790f2ebc56df946dcca2e52e118fe
SergeS82 <bazantmail@gmail.com>
2022-10-26 20:52:53 +0300

    прохождение модуля 21

--- app/src/main/java/ru/anudx/project_kino/MainActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/MainActivity.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import ru.anudx.project_kino.databinding.ActivityMainBinding
+import ru.anudx.project_kino.databinding.BarLayoutTestBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val bam = ActivityMainBinding.inflate(layoutInflater)
+//        val bam = ActivityMainBinding.inflate(layoutInflater)
+        val bam = BarLayoutTestBinding.inflate(layoutInflater)
         setContentView(bam.root)
-        bam.btnMenu.setOnClickListener { Toast.makeText(this, R.string.btn_menu, Toast.LENGTH_SHORT).show() }
-        bam.btnFavorites.setOnClickListener { Toast.makeText(this, R.string.btn_favorites, Toast.LENGTH_SHORT).show() }
-        bam.btnLate.setOnClickListener { Toast.makeText(this, R.string.btn_later, Toast.LENGTH_SHORT).show() }
-        bam.btnSelections.setOnClickListener { Toast.makeText(this, R.string.btn_selections, Toast.LENGTH_SHORT).show() }
-        bam.btnSettings.setOnClickListener { Toast.makeText(this, R.string.btn_settings, Toast.LENGTH_SHORT).show() }
+//        bam.btnMenu.setOnClickListener { Toast.makeText(this, R.string.btn_menu, Toast.LENGTH_SHORT).show() }
+//        bam.btnFavorites.setOnClickListener { Toast.makeText(this, R.string.btn_favorites, Toast.LENGTH_SHORT).show() }
+//        bam.btnLate.setOnClickListener { Toast.makeText(this, R.string.btn_later, Toast.LENGTH_SHORT).show() }
+//        bam.btnSelections.setOnClickListener { Toast.makeText(this, R.string.btn_selections, Toast.LENGTH_SHORT).show() }
+//        bam.btnSettings.setOnClickListener { Toast.makeText(this, R.string.btn_settings, Toast.LENGTH_SHORT).show() }
     }
 }
