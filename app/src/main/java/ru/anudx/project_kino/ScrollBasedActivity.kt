4197c56717cca308f835743fbf4f502db9da639a
SergeS82 <bazantmail@gmail.com>
2022-11-11 22:58:18 +0300

    CoordinatorLayout: добавлен Snackbar

--- app/src/main/java/ru/anudx/project_kino/ScrollBasedActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/ScrollBasedActivity.kt
@@ -2,6 +2,7 @@ package ru.anudx.project_kino
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import ru.anudx.project_kino.databinding.ScrollBasedActivityBinding
 
 class ScrollBasedActivity: AppCompatActivity() {
@@ -9,5 +10,13 @@ class ScrollBasedActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val b = ScrollBasedActivityBinding.inflate(layoutInflater)
         setContentView(b.root)
+        val snackbar = Snackbar.make(b.root, "Snackbar!", Snackbar.LENGTH_SHORT)
+        b.fab2.setOnClickListener {
+            when (snackbar.isShown){
+                true -> snackbar.dismiss()
+                else -> snackbar.show()
+            }
+        }
+
     }
 }
