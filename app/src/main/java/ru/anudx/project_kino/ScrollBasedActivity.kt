78c78fff13c760b1a758b9f77c6d1ed85dbca79e
SergeS82 <bazantmail@gmail.com>
2022-11-11 23:04:25 +0300

    CoordinatorLayout: Snackbar - цвет действия

--- app/src/main/java/ru/anudx/project_kino/ScrollBasedActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/ScrollBasedActivity.kt
@@ -3,6 +3,7 @@ package ru.anudx.project_kino
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import ru.anudx.project_kino.databinding.ScrollBasedActivityBinding
 
@@ -12,6 +13,7 @@ class ScrollBasedActivity: AppCompatActivity() {
         val b = ScrollBasedActivityBinding.inflate(layoutInflater)
         setContentView(b.root)
         val snackbar = Snackbar.make(b.root, "Snackbar!", Snackbar.LENGTH_SHORT)
+        snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.teal_200))
         snackbar.setAction("Action"){
             Toast.makeText(this, "Toast!", Toast.LENGTH_SHORT).show()
         }
