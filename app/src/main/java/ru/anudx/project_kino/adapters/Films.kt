9e066c6ab01a47fcf53c24eb1108f4f77deba91c
SergeS82 <bazantmail@gmail.com>
2022-11-12 21:18:32 +0300

    добавлен RecyclerView и карточка

---
+++ app/src/main/java/ru/anudx/project_kino/adapters/Films.kt
@@ -0,0 +1,23 @@
+package ru.anudx.project_kino.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Films: RecyclerView.Adapter<Films.ViewHolder> {
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/ru/anudx/project_kino/model/Films.kt
@@ -0,0 +1,3 @@
+package ru.anudx.project_kino.model
+
+data class Films(val id: Int, val title: String, val description: String)
