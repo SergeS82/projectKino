b81251a8827da67b7d80a6cf59f4aee949b910df
SergeS82 <bazantmail@gmail.com>
2022-11-12 22:58:58 +0300

    добавил черезстрочную декорацию

--- app/src/main/java/ru/anudx/project_kino/decorations/RecyclerDecoration.kt
+++ app/src/main/java/ru/anudx/project_kino/decorations/RecyclerDecoration.kt
@@ -1,11 +1,17 @@
 package ru.anudx.project_kino.decorations
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Rect
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.google.android.material.internal.ViewUtils.RelativePadding
+import ru.anudx.project_kino.R
+import ru.anudx.project_kino.databinding.FilmsItemBinding
 
 class RecyclerDecoration(val context: Context, val sidePadding: Int, val topPadding: Int): ItemDecoration(){
     override fun getItemOffsets(
@@ -19,5 +25,18 @@ class RecyclerDecoration(val context: Context, val sidePadding: Int, val topPadd
         outRect.bottom = topPadding
         outRect.left = sidePadding
         outRect.right = sidePadding
+    }
+
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        parent.forEachIndexed { i, view ->
+            val position = parent.getChildAdapterPosition(view)
+            val b = FilmsItemBinding.bind(view)
+            if (position % 2 == 0){
+                b.constraint.background = ResourcesCompat.getDrawable(context.resources, R.color.card_background_light, context.theme)
+            }
+            else{
+                b.constraint.background = ResourcesCompat.getDrawable(context.resources, R.color.card_background_dark, context.theme)
+            }
+        }
     }
 }
