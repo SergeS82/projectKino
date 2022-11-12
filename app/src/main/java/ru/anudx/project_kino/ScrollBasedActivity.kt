75d3e84a60da1c915cc28ccf4225fbc6b38300fe
SergeS82 <bazantmail@gmail.com>
2022-11-12 19:40:35 +0300

    просто коммит

--- app/src/main/java/ru/anudx/project_kino/ScrollBasedActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/ScrollBasedActivity.kt
@@ -5,8 +5,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.size
-import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
 import ru.anudx.project_kino.databinding.ScrollBasedActivityBinding
