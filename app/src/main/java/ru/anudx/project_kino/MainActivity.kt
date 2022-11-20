abd57cdedf92e48840b4fc32e88be1ce42fe1811
SergeS82 <bazantmail@gmail.com>
2022-11-20 21:31:32 +0300

    переместил LifecycleObserver в companion object App

--- app/src/main/java/ru/anudx/project_kino/MainActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/MainActivity.kt
@@ -22,10 +22,9 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b: ActivityMainBinding
-    private val lifeCycleListener = MainActivityLifeCycleListener()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycle.addObserver(lifeCycleListener)
+        lifecycle.addObserver(App.lifeCycleListener)
         Log.d("debug_info", App.instance.objectInAppClass)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
