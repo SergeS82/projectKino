da61486790114c602bc63e4ce57bf19214beab69
SergeS82 <bazantmail@gmail.com>
2022-11-20 20:53:06 +0300

    добавил : Application

--- app/src/main/java/ru/anudx/project_kino/App.kt
+++ app/src/main/java/ru/anudx/project_kino/App.kt
@@ -1,13 +1,16 @@
 package ru.anudx.project_kino
 
 import android.app.Application
-import timber.log.Timber
 
 class App: Application() {
+    val objectInAppClass = "Object"
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG){
-            Timber.plant(Timber.DebugTree())
-        }
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+            private set
     }
 }
--- app/src/main/java/ru/anudx/project_kino/MainActivity.kt
+++ app/src/main/java/ru/anudx/project_kino/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var b: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("debug_info", App.instance.objectInAppClass)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
         initNavigation()
