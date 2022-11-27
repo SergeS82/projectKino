453a35db37610ea35535a3eb5d9ea8e3dbfa3245
SergeS82 <bazantmail@gmail.com>
2022-11-27 18:19:50 +0300

    убрал лишнее + логирование

--- app/src/main/java/ru/anudx/project_kino/fragments/DetailFragment.kt
+++ app/src/main/java/ru/anudx/project_kino/fragments/DetailFragment.kt
@@ -12,16 +12,7 @@ import ru.anudx.project_kino.R
 import ru.anudx.project_kino.databinding.FragmentDetailBinding
 import ru.anudx.project_kino.model.FilmsData
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment() : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -32,10 +23,6 @@ class DetailFragment() : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move).apply { duration = 1000 }
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -52,22 +39,9 @@ class DetailFragment() : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
-}
