1b12ec9aff6a5cd2939eb73d7be94dec1754fe32
SergeS82 <bazantmail@gmail.com>
2022-12-11 16:18:40 +0300

    модуль 27: предварительно

--- app/src/main/java/ru/anudx/project_kino/study/StudyUserInputFragment.kt
+++ app/src/main/java/ru/anudx/project_kino/study/StudyUserInputFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.anudx.project_kino.R
-import ru.anudx.project_kino.databinding.FragmentDetailBinding
 import ru.anudx.project_kino.databinding.FragmentStudyUserInputBinding
 
 class StudyUserInputFragment : Fragment() {
