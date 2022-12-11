230aa37ad44880f50b88f9cd356a073889b92e9d
SergeS82 <bazantmail@gmail.com>
2022-12-11 18:22:55 +0300

    TextIputLayout: продвинутая маска на номер телефона

--- app/src/main/java/ru/anudx/project_kino/study/StudyUserInputFragment.kt
+++ app/src/main/java/ru/anudx/project_kino/study/StudyUserInputFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.redmadrobot.inputmask.MaskedTextChangedListener
 import ru.anudx.project_kino.R
 import ru.anudx.project_kino.databinding.FragmentStudyUserInputBinding
 
@@ -23,6 +24,14 @@ class StudyUserInputFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_study_user_input, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val listener = MaskedTextChangedListener("+7 ([000]) [000]-[00]-[00]", b.phoneNumber)
+        b.phoneNumber.addTextChangedListener(listener)
+        b.phoneNumber.onFocusChangeListener = listener
+
     }
 
     companion object {
